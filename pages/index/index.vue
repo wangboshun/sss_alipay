22b9345a93b34684b9a84c5e6f848d4b0cb211a3
wbs <512742341@qq.com>
2019-09-09 17:31:04 +0800

    init

---
+++ pages/index/index.vue
@@ -0,0 +1,18 @@
+<template>
+	<view class="uni-padding-wrap uni-common-mt">
+		<view class="uni-flex uni-column">
+			<view class="uni-col-10 uni-bg-red uni-height-500 ">A</view>
+
+			<view class="uni-col-10 uni-bg-red uni-height-50 uni-top-100">A</view>
+		</view>
+	</view>
+</template>
+<script>
+export default {
+	data() {
+		return {};
+	}
+};
+</script>
+
+<style></style>
