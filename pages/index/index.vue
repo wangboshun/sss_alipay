10e70ff72d5ce8a9906e9883362c6de036d93793
wbs <512742341@qq.com>
2019-09-10 13:42:08 +0800

    main page

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,9 +1,61 @@
 <template>
-	<view class="uni-padding-wrap uni-common-mt">
-		<view class="uni-flex uni-column">
-			<view class="uni-col-10 uni-bg-red uni-height-500 ">A</view>
+	<!--<view class="uni-padding-wrap uni-common-mt uni-height-500">
+		 	<view class="uni-flex uni-column">
+			<view class="uni-col-5 uni-bg-red uni-height-500 ">A</view>
 
-			<view class="uni-col-10 uni-bg-red uni-height-50 uni-top-100">A</view>
+			<view class="uni-col-5 uni-bg-red uni-height-50 uni-top-100">B</view>
+
+			<view class="uni-col-5 uni-bg-red uni-height-50 uni-top-100">C</view>
+		</view>
+
+		<view class="uni-flex uni-row uni-flex-wrap">
+			<view class="uni-col-3 uni-bg-red uni-height-100 uni-top-10">正</view>
+
+			<view class="uni-col-4 uni-bg-red uni-height-50 uni-left-10 uni-top-10">在</view>
+
+			<view class="uni-col-4 uni-bg-red uni-height-50 uni-left-10 uni-top-10">换</view>
+
+			<view class="uni-col-4 uni-bg-red uni-height-50 uni-left-10 uni-top-10">行</view>
+		</view>
+
+		<view class="uni-flex uni-row uni-flex-wrap uni-justify-content-between">
+			<view class="uni-col-3 uni-bg-red uni-height-100 uni-top-10 ">左</view>
+
+			<view class="uni-col-4 uni-bg-red uni-height-50 uni-left-10 uni-top-10 ">右</view>
+		</view>
+
+		<view class="uni-flex uni-row uni-flex-wrap uni-justify-content-between">
+			<view class="uni-col-3 uni-bg-red uni-height-100 uni-top-10  uni-order-4">左 排序4</view>
+
+			<view class="uni-col-4 uni-bg-red uni-height-50 uni-left-10 uni-top-10  uni-order-1">右 排序1</view>
+		</view>
+		
+		
+		<view class="uni-flex uni-row uni-flex-wrap"  style="align-content: flex-start;">
+			<view class="uni-col-3 uni-bg-red uni-height-100 uni-top-10">正</view>
+		
+			<view class="uni-col-4 uni-bg-red uni-height-50 uni-left-10 uni-top-10">在</view>
+		
+			<view class="uni-col-4 uni-bg-red uni-height-50 uni-left-10 uni-top-10">换</view>
+		
+			<view class="uni-col-4 uni-bg-red uni-height-50 uni-left-10 uni-top-10">行</view>
+		</view> 
+	</view>-->
+	<view>
+		<view class="uni-flex uni-row uni-justify-content-between uni-header uni-col-10  uni-height-100">
+			<view class="uni-col-3 uni-bg-red uni-text-center ">顶</view>
+			<view class="uni-col-3 uni-bg-red uni-text-center">部</view>
+		</view>
+		<view class="uni-flex uni-column">
+			<image src="../../static/qrcode.png" class="aspectFill" style="margin-top: 100upx;"></image>
+			<view style="height: 500px">内容</view>
+			<view style="height: 500px">内容</view>
+			<view style="height: 500px">内容</view>
+		</view>
+
+		<view class="uni-flex uni-row uni-justify-content-between uni-footer uni-col-10  uni-height-100">
+			<view class="uni-col-3 uni-bg-red uni-text-center">底</view>
+			<view class="uni-col-3 uni-bg-red uni-text-center">部</view>
 		</view>
 	</view>
 </template>
@@ -11,6 +63,9 @@
 export default {
 	data() {
 		return {};
+	},
+	onLoad() {
+		 
 	}
 };
 </script>
